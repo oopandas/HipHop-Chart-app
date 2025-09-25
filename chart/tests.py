0d530b436a4946bb7435810cfeb4c6d1611de1b4
熊谷駿 <kumagaishun@kumagaishunnoMacBook-Air.local>
2025-09-25 13:56:27 +0900

    機能: API失敗時にモックデータを返す処理を追加

--- chart/tests.py
+++ chart/tests.py
@@ -1,18 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse
 from unittest.mock import patch
+import requests
 
 class ChartViewTests(TestCase):
-
     @patch("chart.views.requests.get") # requests.getをモック
     def test_fallback_to_mock_data_on_api_failure(self, mock_get):
         #API呼び出しが失敗したことにする
-        mock_get.side_offect = Exception("API down")
+        mock_get.side_effect = requests.RequestException("API down")
 
         # /chart/ にアクセス
         response = self.client.get(reverse("chart:chart_list"))
-        self.assertEqual(response.status_code, 200)
-
+        # self.assertEqual(response.status_code, 200)
+        print("response.context:", response.content) # デバック
+        print(response.content.decode())             # デバック
         # モックデータが表示されているかを確認
         self.assertContains(response, "Mock Song 1")
         self.assertContains(response, "Mock Artist A")
